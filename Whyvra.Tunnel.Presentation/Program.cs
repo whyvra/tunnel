a9a4e0b7b96c694996f57769b412828bb28fb3e2
whyvra <iamwhyvra@gmail.com>
2020-10-28 09:39:58 -0400

    Added Blazor for presentation layer

---
+++ Whyvra.Tunnel.Presentation/Program.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Whyvra.Tunnel.Presentation
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var builder = WebAssemblyHostBuilder.CreateDefault(args);
+            builder.RootComponents.Add<App>("app");
+
+            builder.Services.AddScoped(_ => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+            await builder.Build().RunAsync();
+        }
+    }
+}

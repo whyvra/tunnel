33748160d053bc99c73df90d2802703f5f63f761
whyvra <iamwhyvra@gmail.com>
2020-11-19 08:21:32 -0500

    Added redirect and removed delete button from new server form

--- Whyvra.Tunnel.Presentation/Shared/ServerForm.razor.cs
+++ Whyvra.Tunnel.Presentation/Shared/ServerForm.razor.cs
@@ -173,7 +173,7 @@ namespace Whyvra.Tunnel.Presentation.Shared
             }
         }
 
-        private async Task ProcessDelete()
+        protected async Task ProcessDelete()
         {
             IsLoading = true;
             ShowDeleteConfirm = false;
